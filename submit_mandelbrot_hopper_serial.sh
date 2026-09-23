5d5498c379738433a89b7caeb9dc5d3cb5c60a9b
lbl59 <lbl59@cornell.edu>
2026-09-23 17:29:08 -0400

    increase timing for serial script

--- submit_mandelbrot_hopper_serial.sh
+++ submit_mandelbrot_hopper_serial.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=mandelbrot_scaling_hopper_serial
 #SBATCH --output=logs/mandelbrot_scaling_hopper_serial.out
 #SBATCH --error=logs/mandelbrot_scaling_hopper_serial.err
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --mail-user=lbl59@cornell.edu
 #SBATCH --mail-type=ALL
 
